dfb848add237253af32c930ad38ec7c401c45301
sbatni <shubha.batni@harmonicinc.com>
2015-06-16 16:06:17 -0700

    Fixed according to Sergii's comments
    1. Removed code duplication inside _createEventHandlers
    2. Removed comments and global context search
    3. simplified if else code
    4. Fixed form validations

--- prodWidget.js
+++ prodWidget.js
@@ -1,26 +1,19 @@
-
 (function($) {
     var VALIDATORS = {
         required: function(value) {
             return !!value;
         },
-        unique: function(value, options, selectedIndex) {
+        unique: function(value, options) {
             for (var key=0, size=options.items.length; key<size;key++) {
-                if(options.items[key][options.fieldName] === value) {
-                    if(selectedIndex == -1) {
-                        return false;
-                    }
-                    else if (key !== selectedIndex) {
-                        return false;
-                    }
-                    
+                if(options.items[key][options.fieldName] === value && key !== options.selectedIndex) {
+                    return false;
                 }
             }
             return true;
         },
         number: function(value) {
             if(!!value) {
-                return !isNaN(Number(value));
+                return (!isNaN(Number(value)) && value > 0);
             }
             return false;
         }
@@ -28,16 +21,11 @@
 
     var FORMATTER = {
         currencyFormatter: function(price) {
-            var neg = false;
-            if(price < 0) {
-                neg = true;
-                price = Math.abs(price);
-            }
-            return (neg ? "-$" : '$') + parseFloat(price, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();
+            return '$' + parseFloat(price, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,').toString();
         }
     }
 
-    $.widget("custom.prodWidget", {
+    $.widget('custom.prodWidget', {
         _init: function() {
         },
 
@@ -46,18 +34,15 @@
             this._rowTemplate = this._grid.find('[data-part=grid-row]').remove();
             this._form = this.element.find('[data-part=form]').get(0);
             this._items = [];
-            this._counter = 1;
+            this._productId = 1;
             this._selectedIndex = -1;
             this._itemFormatters = {'price': 'currencyFormatter'};
-            this._rulesSet = {'name': { required: true },
-                            'sku': { required: true, unique: { items: this._items, fieldName: 'sku' } },
-                            'price': { required: true, number: true }};
 
             this._on(this._grid, {
                 'click .delete': function(event) {
-                    if(confirm("Delete selected product?")) {
+                    if(confirm('Delete selected product?')) {
                         var index = $(event.target).closest('tr').index();
-                        $("tbody tr:eq(" + index + ")").remove();
+                        this._grid.find('tbody tr').eq(index).remove();
                         this._items.splice(index, 1);
                     }
                 },
@@ -65,16 +50,14 @@
                 'click .edit': function(event) {
                     this._selectedIndex = $(event.target).closest('tr').index();
                     var product = this.getItem(this._selectedIndex);
-                    this._trigger("editing", event, product);
+                    this._trigger('editing', event, product);
                 }
             });
-            var _self = this;
-            $.each(this._rulesSet, function(inputName, validators) {
-                _self.validates(inputName, validators);
-            });
-            /*this.validates('name', { required: true });
-            this.validates('sku', { required: true, unique: { items: this._items, fieldName: 'sku' } });
-            this.validates('price', { required: true, number: true });*/
+
+            this.validates('name', { required: true });
+            this.validates('sku', { required: true, unique: { items: this._items, fieldName: 'sku',
+                                    selectedIndex: this._selectedIndex } });
+            this.validates('price', { required: true, number: true });
         },
 
         validates: function(fieldName, validators) {
@@ -83,87 +66,55 @@
         },
 
         _createEventHandlersFor: function(inputName, validators) {
-            //call one more function as we are passing arguments
             return function(event) {
                 var _self = this;
                 var value = event.target.value;
-                var fieldName;
+                var field = $(event.target);
+                var errorField = field.parent().find('span');
 
-                //fieldName = $('[name="' + inputName + '"]');
-                fieldName = $(event.target);
-                //var error_element=$("span", fieldName.parent());
-                var error_element=fieldName.parent().find("span");
-                var enableSave = true;
                 $.each(validators, function(validatorName, options) {
-                    var isValid = VALIDATORS[validatorName](value, options, _self._selectedIndex);
-                    if(isValid) {
-                        fieldName.removeClass("invalid").addClass("valid");
-                        error_element.removeClass("error_show").addClass("error");
-                    }
-                    else {
-                        fieldName.removeClass("valid").addClass("invalid");
-                        error_element.removeClass("error").addClass("error_show");
-                        enableSave = false;
+                    var isValid = VALIDATORS[validatorName](value, options);
+                    _self._setStyle(isValid, field, errorField);
+                    if(!isValid) {
                         return false;
                     }
                 })
-                if(enableSave) {
-                    this._form.save.disabled = false;
-                }
-                else {
-                    this._form.save.disabled = true;
-                }
+                var invalid = (this.element.find('span.error').length > 0 ||
+                                    this.element.find('span.init').length > 0);
+                this._form.save.disabled = invalid;
             };
+        },
+
+
+        _setStyle: function(success, field, errorField) {
+            if(success) {
+                field.removeClass('invalid').addClass('valid');
+                errorField.removeClass('error').removeClass('init').addClass('success');
+            }
+            else {
+                field.removeClass('valid').addClass('invalid');
+                errorField.removeClass('success').removeClass('init').addClass('error');
+            }
         },
 
         getItem : function(index) {
             return this._items[index];
         },
 
-        isValid: function(product) {
-            var enableSave = true;
-            var _self = this;
-            $.each(this._rulesSet, function(inputName, validators) {
+        saveItem: function(product) {
+            event.preventDefault();
 
-                var value = product[inputName];
-                var fieldName = $('[name="' + inputName + '"]');
-                var error_element=$("span", fieldName.parent());
-                $.each(validators, function(validatorName, options) {
-                    var isValid = VALIDATORS[validatorName](value, options, _self._selectedIndex);
-                    if(isValid) {
-                        fieldName.removeClass("invalid").addClass("valid");
-                        error_element.removeClass("error_show").addClass("error");
-                    }
-                    else {
-                        fieldName.removeClass("valid").addClass("invalid");
-                        error_element.removeClass("error").addClass("error_show");
-                        enableSave = false;
-                        return false;
-                    }
-                })
-            });
-            return enableSave;
-        },
-
-        addItem : function(product) {
-            if (this.isValid(product)) {
-                //adding a product
-                if(this._selectedIndex === -1) {
-                    product.id = this._counter;
-                    this._counter++;
-                }
+            if (!product.id) {
+                product.id = this._productId;
+                this._productId++;
                 this._grid.append(this._rowFor(product));
                 this._items.push(product);
-                this._form.save.disabled = true;
-                this._selectedIndex = -1;
             }
-        },
-
-        updateItem : function(product) {
-            this._grid.find('tbody tr').eq(this._selectedIndex).replaceWith(this._rowFor(product));
-
-            //$("tbody tr:eq(" + index + ")").replaceWith(this._rowFor(product));
-            this._items.splice(this._selectedIndex, product);
+            else {
+                this._grid.find('tbody tr').eq(this._selectedIndex).replaceWith(this._rowFor(product));
+                this._items[this._selectedIndex] = product;
+            }
+            this.resetFormFields();
             this._form.save.disabled = true;
             this._selectedIndex = -1;
         },
@@ -173,9 +124,7 @@
             var newNode = this._rowTemplate.clone();
 
             var html = newNode.html().replace(/\{\{\s*(\w+)\s*\}\}/g, function(match, capture) {
-                //check if any formatters are defined for each key
-                if(_self._itemFormatters[capture])
-                {
+                if(_self._hasFormatterFor(capture)) {
                     return FORMATTER[_self._itemFormatters[capture]](product[capture]);
                 }
                 return product[capture];
@@ -183,6 +132,37 @@
 
             return newNode.html(html);
         },
+
+        _hasFormatterFor: function(field) {
+            var _self = this;
+            return _self._itemFormatters[field];
+        },
+
+        productFromForm : function() {
+            var self = this;
+            var product = 'name sku price id'.split(' ').reduce(function(product, fieldName) {
+                product[fieldName] = self._form[fieldName].value;
+                return product;
+            }, {});
+
+            return product;
+        },
+
+        productToForm: function(object) {
+            var field;
+            for (var key in object) {
+                field = this._form[key];
+                field.value = object[key];
+            }
+        },
+
+        resetFormFields: function() {
+            this._form.name.value = '';
+            this._form.sku.value = '';
+            this._form.price.value = '';
+            this._form.id.value = '';
+            $(this._form.span).removeClass('error').removeClass('success').addClass('init');
+        }
 
     });
 }(jQuery));
--- script.js
+++ script.js
@@ -3,42 +3,15 @@
  */
 $(function() {
 
-    $( "#prodManagement" ).prodWidget();
+    $( '#prodManagement' ).prodWidget();
 
-    $("[name=save]").disabled = true;
-
-    $("[name=save]").click(function(event){
-
-        var product = {
-            sku: $('[name="sku"]').val(),
-            name: $('[name="name"]').val(),
-            price: $('[name="price"]').val(),
-            id: $('[name="id"]').val()
-        };
-        var id = $('[name="prodid"]').val();
-        if(!id) {
-            $( "#prodManagement" ).prodWidget("addItem", product);
-        }
-        else
-        {
-            $( "#prodManagement" ).prodWidget("updateItem", product);
-        }
-
-        $('[name="name"]').val("");
-        $('[name="sku"]').val("");
-        $('[name="price"]').val("");
-        $('[name="prodid"]').val("");
-
-        event.preventDefault();
-        event.stopPropagation();
-
+    $('[name=save]').click(function(event) {
+        var product = $( '#prodManagement').prodWidget('productFromForm');
+        $( '#prodManagement' ).prodWidget('saveItem', product);
     });
 
-    $( "#prodManagement" ).on( "prodwidgetediting", function( event, product) {
-        $('[name="name"]').val(product.name);
-        $('[name="sku"]').val(product.sku);
-        $('[name="price"]').val(product.price);
-        $('[name="prodid"]').val(product.id);
+    $( '#prodManagement' ).on( 'prodwidgetediting', function( event, product) {
+        $( '#prodManagement').prodWidget('productToForm', product);
     });
 
 });
